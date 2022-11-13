1914268d0693908e37a6272b89193b3bdfad07c8
marloupepito <marloupepito07@gmail.com>
2022-11-13 09:40:53 +0800

    add policy

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,6 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"> 
     <script src="{{ asset('js/app.js') }}" defer></script>
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
         <title>Laravel</title>
