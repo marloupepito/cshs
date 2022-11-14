f118c17c71d0ccb36c5c91dcbc4390853016af35
marloupepito <marloupepito07@gmail.com>
2022-11-14 23:06:11 +0800

    Adding validation controller

--- app/Http/Controllers/RegistrationController.php
+++ app/Http/Controllers/RegistrationController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Auth;
 class RegistrationController extends Controller
